2c7e97e0b425bfe000da5e01fbed3a192db86693
resin8or <jamie.tobin@gmail.com>
2021-03-23 19:31:37 +0000

    Amended to use Spring Component, AutoWired and App Context.

--- src/main/java/com/res/spring/basics/springdemo/QuickSortAlgorithm.java
+++ src/main/java/com/res/spring/basics/springdemo/QuickSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.res.spring.basics.springdemo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm{
 
     public int[] sort(int[] numbers){
--- src/main/java/com/res/spring/basics/springdemo/SpringDemoApplication.java
+++ src/main/java/com/res/spring/basics/springdemo/SpringDemoApplication.java
@@ -2,23 +2,28 @@ package com.res.spring.basics.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 
 	// Key Q's that the Spring framework needs to know....
-	// what are the beans?
-	// what are the dependencies of a bean?
-	// where to search for beans?
+	// what are the beans? Used @Component
+	// what are the dependencies of a bean? Used @AutoWired
+	// where to search for beans? By default will search same pkg and subpkg as main
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
 
+		ApplicationContext appContext =
+				SpringApplication.run(SpringDemoApplication.class, args);
+
+		BinarySearchImpl binarySearch = appContext.getBean(BinarySearchImpl.class);
+
+		// Spring Application Context will manage and provide access to all beans.
 		int result = binarySearch.binarySearch(new int[] {2, 6, 3, 4},3);
 
 		System.out.println("Result is " + result);
-
-		//SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
 }
