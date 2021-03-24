f3b64e876a7a2ccfede76b33eb0ff7088ab03d28
resin8or <jamie.tobin@gmail.com>
2021-03-24 08:34:58 +0000

    Remove primary qualifier and now working with the naming qualifier.

--- src/main/java/com/res/spring/basics/springdemo/BinarySearchImpl.java
+++ src/main/java/com/res/spring/basics/springdemo/BinarySearchImpl.java
@@ -1,24 +1,29 @@
 package com.res.spring.basics.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl{
 
-    @Autowired
-    private SortAlgorithm sortAlgorithm; // BImpl depends on SortAlgorithm
+//    @Autowired
+//    private SortAlgorithm sortAlgorithm; // BImpl depends on SortAlgorithm
 
-    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-        super();
-        this.sortAlgorithm = sortAlgorithm;
-    }
+//    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+//        super();
+//        this.sortAlgorithm = sortAlgorithm;
+//    }
+
+    @Autowired
+    @Qualifier("bubble")
+    private SortAlgorithm bubbleSortAlgorithm;
 
     public int  binarySearch(int[] numbers, int numbertoSearchFor){
 
         // sorting an array
-        int[] sortedNumbers = sortAlgorithm.sort(numbers);
-        System.out.println("Algorithm: " + sortAlgorithm);
+        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+        System.out.println("Algorithm: " + bubbleSortAlgorithm);
         return 3;
 
         // search the array
