65acb60b1daff9aa55e35d710f1f5e1ad05ecd89
resin8or <jamie.tobin@gmail.com>
2021-03-24 08:14:29 +0000

    Added @Components for the sorts and made BubbleSort the Primary component for Autowired.

--- src/main/java/com/res/spring/basics/springdemo/BubbleSortAlgorithm.java
+++ src/main/java/com/res/spring/basics/springdemo/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.res.spring.basics.springdemo;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-
+@Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm {
 
     public int[] sort(int[] numbers){
