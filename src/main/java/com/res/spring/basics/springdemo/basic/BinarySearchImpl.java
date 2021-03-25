5708d3bf595ee402eec0e15b439bf1845b5aa28e
resin8or <jamie.tobin@gmail.com>
2021-03-25 07:42:03 +0000

    Added @PreDestroy and @PostConstruct examples. Added CDI in the pom. Pre removal of SpringBoot from the application.

--- src/main/java/com/res/spring/basics/springdemo/basic/BinarySearchImpl.java
+++ src/main/java/com/res/spring/basics/springdemo/basic/BinarySearchImpl.java
@@ -1,15 +1,21 @@
 package com.res.spring.basics.springdemo.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl{
 
+    private Logger logger = LoggerFactory.getLogger(BinarySearchImpl.class);
 //    @Autowired
 //    private SortAlgorithm sortAlgorithm; // BImpl depends on SortAlgorithm
 
@@ -32,7 +38,16 @@ public class BinarySearchImpl{
         // search the array
     }
 
-    // return the result
-    
+    @PostConstruct
+    public void postConstruct(){
+        // Once bean created and initialised with dependencies this method will be called.
+        logger.info("PostConstruct firing ... ");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        // This is called just before the bean is removed from ctx.
+        logger.info("PreDestroy firing ... ");
+    }
 
 }
